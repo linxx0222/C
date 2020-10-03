9740512c9909b8f0f74cc7e56cf69bb7345a2ae2
linxx0222 <linyellow0222@gmail.com>
2020-10-03 15:21:05 +0800

    [Add] Check whether number is 2 square number

--- 2_Square/main.c
+++ 2_Square/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int input;
+
+    printf("Please type a number¡G");
+    scanf("%d", &input);
+
+    if (input > 0 && (input & (input - 1)) == 0 )
+    {
+        printf("Input is a 2 Square number\n");
+    }
+    else
+        printf("Input is not a 2 Square number\n");
+
+
     return 0;
 }
