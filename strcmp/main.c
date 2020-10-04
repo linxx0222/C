4ec93284086e9504d2830248d357304a42047157
linxx0222 <linyellow0222@gmail.com>
2020-10-04 23:26:02 +0800

    [Modify] Complete strcmp function

--- strcmp/main.c
+++ strcmp/main.c
@@ -6,15 +6,27 @@ int strncmp_imp ( const char* s1, const char * s2 , size_t n );
 
 int main()
 {
-    char A[10], B[10];
-    char *p1, *p2;
-    p1 = A;
-    p2 = B;
+    char *source = "abc";
+    char *d1 = "abc";
+    char *d2 = "cde";
+
+    printf("strcmp: %d\n", strcmp_imp(source, d1));
+    printf("strcmp: %d\n", strcmp_imp(source, d2));
 
     return 0;
 }
 
 int strcmp_imp ( const char* s1, const char * s2 )
 {
-
+    while(*s1 != '\0' || *s2 != '\0')
+    {
+        if((*s1 - *s2) > 0)
+            return (*s1 - *s2);
+        else if((*s1 - *s2) < 0)
+            return (*s1 - *s2);
+        else
+            s1++;
+            s2++;
+    }
+    return 0;
 }
