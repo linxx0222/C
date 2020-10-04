66790d41a9edd86b424b7101f2bf77cd2a027dc9
linxx0222 <linyellow0222@gmail.com>
2020-10-04 23:05:40 +0800

    [New] New String compare function

---
+++ strcmp/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int strcmp_imp ( const char* s1, const char * s2 );
+int strncmp_imp ( const char* s1, const char * s2 , size_t n );
+
+int main()
+{
+    char A[10], B[10];
+    char *p1, *p2;
+    p1 = A;
+    p2 = B;
+
+    return 0;
+}
+
+int strcmp_imp ( const char* s1, const char * s2 )
+{
+
+}
