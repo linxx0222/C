c294847a336258de66bc64e226c1f6908bd9b928
linxx0222 <linyellow0222@gmail.com>
2021-01-07 22:05:55 +0800

    [Add] Add Greatest Common Divisor

---
+++ Greatest Common Divisor/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int GCD(int a, int b)
+{
+    if(a % b == 0)
+    {
+        return b;
+    }
+    else
+    {
+        return GCD(b, a % b);
+    }
+}
+
+int main()
+{
+    printf("Greatest Common Divisor! (GCD)\n");
+    int a, b;
+    int result;
+    printf("Type 2 numbers:\n");
+    scanf("%d %d", &a, &b);
+
+    result = GCD(a,b);
+    printf("Result: %d", result);
+
+    return 0;
+}
