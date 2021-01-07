850938fc64eb80cad9696647853ec1eb7e1d31bb
linxx0222 <linyellow0222@gmail.com>
2021-01-07 18:39:06 +0800

    [Add] Complete Quick Sort

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void QuickSort(int List[], int left, int right)
+{
+    int pivot;
+    int i, j;
+    if (left < right)
+    {
+        pivot = List[left];
+        i = left + 1;
+        j = right;
+        while(i < j)
+        {
+            while(List[i] < pivot)
+                i++;
+            while(List[j] > pivot)
+                j--;
+            if(i < j)
+                swap(&List[i], &List[j]);
+        }
+
+        swap(&List[left], &List[j]);
+        QuickSort(List, left, j - 1);
+        QuickSort(List, j + 1, right);
+    }
+}
+
 int main()
 {
     printf("Quick Sort\n");
@@ -8,7 +40,7 @@ int main()
     int i, j, temp;
     int n = sizeof(List)/sizeof(int);
 
-    QuickSort(List, n);
+    QuickSort(List, 0, n - 1);
 
     for(i = 0; i < n; i++)
     {
