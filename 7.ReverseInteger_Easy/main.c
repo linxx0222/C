d137fe1e9f6c4be3216cf37826010ce280f497ac
linxx0222 <linyellow0222@gmail.com>
2021-03-19 00:12:21 +0800

    [New] New 7. Reverse Integer - Easy

---
+++ 7.ReverseInteger_Easy/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int reverse(int x){
+    if(x == 0 )
+        return 0;
+
+    int i = 0;
+    long long temp = x;
+    int negative = 0;
+
+    if(x < 0)
+    {
+        negative = 1;
+        temp = -x;
+    }
+
+    while(temp != 0)
+    {
+        temp = temp / 10;
+        i++;
+    }
+
+    int degree = i;
+    int arr[degree];
+
+    if(x < 0)
+    {
+        negative = 1;
+        temp = -x;
+    }
+    else
+        temp = x;
+
+    int j = 0;
+    while(temp != 0)
+    {
+        arr[j] = temp % 10;
+        temp = temp / 10;
+        j++;
+    }
+
+    long result = 0;
+    int k;
+    k = degree - 1;
+    for(j = 0; j < degree; j++)
+    {
+        result += arr[j] * pow(10, k);
+        k--;
+    }
+    if(negative)
+        result = -result;
+
+    if(result > INT_MAX || result  < INT_MIN ) return 0;
+
+    return result;
+}
