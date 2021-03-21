4367d63e6584e92baaea1a86b58d8af1a2efa0d6
linxx0222 <linyellow0222@gmail.com>
2021-03-21 19:19:08 +0800

    [New] New 326. Power of Three - Easy

---
+++ 326.PowerOfThree_Easy/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+bool isPowerOfThree(int n){
+    if(n <= 0)
+        return false;
+
+    while(n > 1)
+    {
+        if(n % 3)
+            return false;
+        n /= 3;
+    }
+    return true;
+}
